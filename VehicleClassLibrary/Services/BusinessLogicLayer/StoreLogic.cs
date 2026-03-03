b6f4e466896aa4bb16782436a7d89e2329ff279a
Etendo <etendo123@gmail.com>
2026-03-02 18:43:28 -0700

    Added the StoreLogic class to pass data to and from the DAO presentation layers.

---
+++ VehicleClassLibrary/Services/BusinessLogicLayer/StoreLogic.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleClassLibrary.Models;
+using VehicleClassLibrary.Services.DataAccessLayer;
+
+namespace VehicleClassLibrary.Services.BusinessLogicLayer
+{
+    public class StoreLogic
+    {
+        // Declare class level variables
+        private StoreDAO _storeDAO;
+
+        /// <summary>
+        /// Default constructor for StoreLogic
+        /// </summary>
+        public StoreLogic()
+        {
+            // Initialize the DAO instance
+            _storeDAO = new StoreDAO();
+        }
+
+        /// <summary>
+        /// Get a list of vehicles in the inventory
+        /// </summary>
+        /// <returns></returns>
+        public List<VehicleModel> GetInventory()
+        {
+            // Call and return the vehicles in the inventory
+            return _storeDAO.GetInventory();
+        }
+
+        /// <summary>
+        /// Get a list of the vehicles in the users shopping cart
+        /// </summary>
+        /// <returns></returns>
+        public List<VehicleModel> GetShoppingCart()
+        {
+            // Call and return the vehicles in the shopping cart
+            return _storeDAO.GetShoppingCart();
+        }
+
+        /// <summary>
+        /// Add a new vehicle to the inventory
+        /// </summary>
+        /// <param name="vehicle"></param>
+        /// <returns></returns>
+        public int AddVehicleToInventory(VehicleModel vehicle)
+        {
+            // Call and return the AddVehicleToInventory method in the DAO
+            return _storeDAO.AddVehicleToInventory(vehicle);
+        }
+
+        /// <summary>
+        /// Add a vehicle to the shopping cart by the vehicles ID
+        /// </summary>
+        /// <param name="vehicleId"></param>
+        /// <returns></returns>
+        public int AddVehicleToCart(int vehicleId)
+        {
+            // Call and return the AddVehicleToCart method in the DAO
+            return _storeDAO.AddVehicleToCart(vehicleId);
+        }
+
+        /// <summary>
+        /// Write the inventory to a text file
+        /// </summary>
+        public void WriteInventory()
+        {
+            // Call the WriteInventory method in the DAO
+            _storeDAO.WriteInventory();
+        }
+
+        /// <summary>
+        /// Read the list of vehicles from a text file
+        /// </summary>
+        /// <returns></returns>
+        public List<VehicleModel> ReadInventory()
+        {
+            // Call and return the ReadInventory method in the DAO
+            return _storeDAO.ReadInventory();
+        }
+
+        /// <summary>
+        /// Get the total of the users shopping cart and clear the cart
+        /// </summary>
+        /// <returns></returns>
+        public decimal Checkout()
+        {
+            // Call and return the Checkout method in the DAO
+            return _storeDAO.Checkout();
+        }
+    }
+}
