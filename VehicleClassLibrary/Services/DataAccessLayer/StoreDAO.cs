752ca8264a2269f70eb632f2d015e1f49dd2c4a6
Etendo <etendo123@gmail.com>
2026-06-27 20:52:23 -0600

    Completed the DAO with read, write, and checkout methods.

--- VehicleClassLibrary/Services/DataAccessLayer/StoreDAO.cs
+++ VehicleClassLibrary/Services/DataAccessLayer/StoreDAO.cs
@@ -96,5 +96,251 @@ namespace VehicleClassLibrary.Services.DataAccessLayer
             // Return the number of items in the shopping cart
             return _shoppingCart.Count;
         }
+
+        /// <summary>
+        /// Write the inventory to a text file
+        /// </summary>
+        /// <returns></returns>
+        public bool WriteInventory()
+        {
+            // Check if the directory exists
+            if (!Directory.Exists(_fileDirectory))
+            {
+                // If the directory does not exist, create it
+                Directory.CreateDirectory(_fileDirectory);
+            }
+
+            // Catch any exceptions with the stream writer
+            try
+            {
+                // Create a new stream writer to write to the file
+                using (StreamWriter writer = new StreamWriter(_filePath))
+                {
+                    // Loop through the inventory list
+                    foreach (VehicleModel vehicle in _inventory)
+                    {
+                        Type vehicleType = vehicle.GetType();
+                        switch (vehicleType.Name)
+                        {
+                            case "CarModel":
+                                // Cast the vehicle to a car
+                                CarModel car = (CarModel)vehicle;
+                                // Write the car to the file
+                                writer.WriteLine($"Car,{car.Make},{car.Model},{car.Year},{car.Price}," +
+                                    $"{car.NumWheels},{car.IsConvertable},{car.TrunkSize}");
+                                break;
+
+                            case "MotorcycleModel":
+                                // Cast the vehicle to a motorcycle
+                                MotorcycleModel motorcycle = (MotorcycleModel)vehicle;
+                                // Write the motorcycle to the file
+                                writer.WriteLine($"Motorcycle,{motorcycle.Make},{motorcycle.Model}," +
+                                    $"{motorcycle.Year},{motorcycle.Price},{motorcycle.NumWheels}," +
+                                    $"{motorcycle.HasSidecar},{motorcycle.SeatHeight}");
+                                break;
+
+                            case "PickupModel":
+                                // Cast the vehicle to a pickup truck
+                                PickupModel truck = (PickupModel)vehicle;
+                                // Write the pickup to the file
+                                writer.WriteLine($"Truck,{truck.Make},{truck.Model},{truck.Year}," +
+                                    $"{truck.Price},{truck.NumWheels}");
+                                break;
+
+                            default:
+                                // Write the vehicle to the file
+                                writer.WriteLine($"Vehicle,{vehicle.Make},{vehicle.Model},{vehicle.Year}," +
+                                    $"{vehicle.Price},{vehicle.NumWheels}");
+                                break;
+                        }
+                    }
+                }
+                // Return true if all the data was saved to the file
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Return false if an exception was thrown
+                return false;
+            }
+
+        } // End of WriteInventory method
+
+        /// <summary>
+        /// Read the list of vehicvles from a text file
+        /// </summary>
+        /// <returns></returns>
+        public List<VehicleModel> ReadInventory()
+        {
+            // Declare and initialize
+            string? line = "";
+            string[] parts = [];
+            string make = "", model = "";
+            int year = 0, numWheels = 0;
+            decimal price = 0m;
+            // Specialty vehicle properties
+            bool isConvertable = false, hasSidecar = false, hasBedCover = false;
+            decimal trunkSize = 0m, seatHeight = 0m, bedSize = 0m;
+
+            // Catch any exceptions with the stream reader
+            try
+            {
+                // Check if the file exists
+                if (File.Exists(_filePath))
+                {
+                    // Create a StreamReader to read the file
+                    using (StreamReader reader = new StreamReader(_filePath))
+                    {
+                        // Read each line of the file
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            // Split the line on a comma-space
+                            // Create a string array to put all the separate vehicle parts into
+                            parts = line.Split(',');
+
+                            // Use the parts array to get the common data (make, model, year, price, numWheels)
+                            make = parts[1];
+                            model = parts[2];
+                            // Parse the year of the vehicle
+                            year = ParseInteger(parts[3]);
+                            // Parse the price of the vehicle
+                            price = ParseDecimal(parts[4]);
+                            // Parse the number of wheels of the vehicle
+                            numWheels = ParseInteger(parts[5]);
+
+                            // Use the first piece of data to create a switch for the specific model
+                            switch (parts[0])
+                            {
+                                case "Car":
+                                    // Parse the convertible status for the car
+                                    isConvertable = ParseBoolean(parts[6]);
+                                    // Parse the trunk size for the car
+                                    trunkSize = ParseDecimal(parts[7]);
+                                    // Create a new car using the read properties
+                                    CarModel car = new CarModel(0, make, model, year, price, numWheels, isConvertable, 
+                                        trunkSize);
+                                    // Add the car to the inventory
+                                    AddVehicleToInventory(car);
+                                    break;
+                                case "Motorcycle":
+                                    // Parse the side car status for the motorcycle
+                                    hasSidecar = ParseBoolean(parts[6]);
+                                    // Parse the seat height for the motorcycle
+                                    seatHeight = ParseDecimal(parts[7]);
+                                    // Create a new motorcycle using the read properties
+                                    MotorcycleModel motorcycle = new MotorcycleModel(0, make, model, year, price,
+                                        numWheels, hasSidecar, seatHeight);
+                                    // Add the motorcycle to the inventory
+                                    AddVehicleToInventory(motorcycle);
+                                    break;
+                                case "Truck":
+                                    // Parse the bed cover status for the pickup truck
+                                    hasBedCover = ParseBoolean(parts[6]);
+                                    // Parse the bed size for the pickup truck
+                                    bedSize = ParseDecimal(parts[7]);
+                                    // Create a new pickup truck using the read properties
+                                    PickupModel truck = new PickupModel(0, make, model, year, price, numWheels,
+                                        hasBedCover, bedSize);
+                                    // Add the pickup truck to the inventory
+                                    AddVehicleToInventory(truck);
+                                    break;
+                                default:
+                                    // Create a new vehicle using the read properties
+                                    VehicleModel vehicle = new VehicleModel(0, make, model, year, price, numWheels);
+                                    // Add the vehicle to the inventory
+                                    AddVehicleToInventory(vehicle);
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                // Return the inventory list as is
+                return _inventory;
+            }
+            // Return the inventory list
+            return _inventory;
+        } // End of ReadInventory method
+
+        /// <summary>
+        /// Method to safely parse an integer
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private int ParseInteger(string input)
+        {
+            try
+            {
+                // Parse the input and return
+                return int.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                // Return 0
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Method to safely parse a decimal
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private decimal ParseDecimal(string input)
+        {
+            try
+            {
+                // Parse the input and return
+                return decimal.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                // Return 0
+                return 0m;
+            }
+        }
+
+        /// <summary>
+        /// Method to safely parse a boolean
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private bool ParseBoolean(string input)
+        {
+            try
+            {
+                // Parse the input and return
+                return bool.Parse(input);
+            }
+            catch (Exception ex)
+            {
+                // Return false
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Get the total of the users shopping cart and clear the cart
+        /// </summary>
+        /// <returns></returns>
+        private decimal Checkout()
+        {
+            // Set up a variable to keep track of the carts total
+            decimal total = 0m;
+
+            // Loop through each vehicle in the shopping cart
+            foreach (VehicleModel vehicle in _shoppingCart)
+            {
+                // Add the vehicle price to the total variable
+                total += vehicle.Price;
+            }
+            // Clear the cart
+            _shoppingCart.Clear();
+            // Return the total
+            return total;
+        }
     }
 }
